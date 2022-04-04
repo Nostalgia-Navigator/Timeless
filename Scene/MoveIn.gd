47423b50e949b725d97fee2d6812bba7362401a7
Alex Chen <achen115@ucr.edu>
2022-04-04 03:02:24 -0700

    refactor MissionBriefing

---
+++ Scene/MoveIn.gd
@@ -0,0 +1,42 @@
+extends Control
+var index = 0
+var items = []
+func _ready():
+	for n in $Content.get_children():
+		$Content.remove_child(n)
+		var p = Control.new()
+		p.add_child(n)
+		var rx = randi()%int(rect_size.x)
+		var ry = randi()%int(rect_size.y)
+		
+		var ext = 1000
+		if randf() < 0.5:
+			if randf() < 0.5:
+				ry = -ext
+			else:
+				ry = rect_size.y + ext
+			p.rect_position = Vector2(rx, ry)
+		else:
+			if randf() < 0.5:
+				rx = -ext
+			else:
+				rx = rect_size.x + ext
+			p.rect_position = Vector2(rx, ry)
+			
+		$Content.add_child(p)
+		items.append(p)
+	
+var time = 0	
+func _process(delta):
+	if index < len(items):
+		if time < 0.25:
+			time += delta
+		else:
+			time = 0
+			index += 1
+	for i in range(0, index):
+		var n = items[i]
+		var d = n.rect_position * 0.1
+		if d.length() > 24:
+			d = d.normalized() * 24
+		n.rect_position -= d
